7355d46715d22c26bfb54816bff8c5465ad31508
egigoka <egigoka@gmail.com>
2021-04-15 15:08:45 +0500

    added cast None to empty string in employee properties fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ except ImportError:
     print("pip3 install git+https://github.com/egigoka/commands")
 
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 # region development functions (temp)
 def print_all_task_fields():
@@ -299,8 +299,11 @@ def check_permissions(b24):
 
 
 def str_or_empty_str_if_none(obj):
+    print(type(obj), str(obj))
     if obj is None:
+        print("ret emp str")
         return ""
+    print("ret", str(obj))
     return str(obj)
 
 
@@ -393,7 +396,9 @@ class BitrixObjects:
             for object_info in sorted_objects:
                 to_print = f"[{cnt}] "
                 for key_sort in self.interactive_selection_sort_by:
-                    to_print += f"{object_info[key_sort]} "
+                    new_str = str(List.itemgetter_with_casting(object_info[key_sort],
+                                                               cast_to=self.interactive_selection_cast_to))
+                    to_print += f"{new_str} "
                 list_for_print.insert(0, to_print)
 
                 enumerated_dict[cnt] = object_info
